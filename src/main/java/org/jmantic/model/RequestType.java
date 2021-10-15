a90b475482e8dc24ef35c536919c460d435a9549
artrayme <artrayme@protonmail.com>
2021-10-16 02:03:15 +0300

    preview request structure

---
+++ src/main/java/org/jmantic/model/RequestType.java
@@ -0,0 +1,12 @@
+package org.jmantic.model;
+
+public enum RequestType {
+    CREATE_ELEMENT,
+    CHECK_ELEMENTS,
+    DELETE_ELEMENTS,
+    SEARCH_ELEMENTS,
+    GENERATE_TEMPLATE,
+    EVENTS,
+    KEYNODES,
+    CONTENT
+}
