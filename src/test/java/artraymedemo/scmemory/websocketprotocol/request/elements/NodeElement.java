cfe23266e1a074f3fa3a3f830f824574188ab32b
artrayme <artrayme@protonmail.com>
2021-10-22 22:25:46 +0300

    create request abstractions

---
+++ src/test/java/artraymedemo/scmemory/websocketprotocol/request/elements/NodeElement.java
@@ -0,0 +1,12 @@
+package artraymedemo.scmemory.websocketprotocol.request.elements;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * @author artrayme
+ * 10/22/21
+ */
+public class NodeElement {
+    @JsonProperty("type")
+    protected int type;
+}
