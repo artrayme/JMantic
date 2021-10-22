a60f66e3414de3cb8109ad9681c59670c1fe11aa
artrayme <artrayme@protonmail.com>
2021-10-22 22:26:24 +0300

    create demo

---
+++ src/test/java/artraymedemo/scmemory/sctypes/ScNode.java
@@ -0,0 +1,15 @@
+package artraymedemo.scmemory.sctypes;
+
+import artraymedemo.scmemory.ScElement;
+
+/**
+ * @author artrayme
+ * 10/22/21
+ */
+public class ScNode implements ScElement {
+
+    @Override
+    public Long getScAddress() {
+        return null;
+    }
+}
