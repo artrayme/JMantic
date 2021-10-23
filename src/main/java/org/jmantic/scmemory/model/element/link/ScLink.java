ed2a295a2389618129ed2958b8375a6e3377a493
artrayme <artrayme@protonmail.com>
2021-10-24 02:07:12 +0300

    add ScLink with available types

---
+++ src/main/java/org/jmantic/scmemory/model/element/link/ScLink.java
@@ -0,0 +1,12 @@
+package org.jmantic.scmemory.model.element.link;
+
+import org.jmantic.scmemory.model.element.ScElement;
+
+/**
+ * @author artrayme
+ * @since 0.0.1
+ */
+public interface ScLink extends ScElement {
+    LinkType getType();
+
+}
