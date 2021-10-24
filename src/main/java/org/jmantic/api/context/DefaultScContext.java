f06237441311f9f4641a8b2ef7b1cc6cd890ce3d
artrayme <artrayme@protonmail.com>
2021-10-24 20:52:40 +0300

    add some context implementation

--- src/main/java/org/jmantic/api/context/DefaultScContext.java
+++ src/main/java/org/jmantic/api/context/DefaultScContext.java
@@ -1,9 +1,27 @@
 package org.jmantic.api.context;
 
+import org.jmantic.scmemory.model.ScMemory;
+import org.jmantic.scmemory.model.element.node.NodeType;
+import org.jmantic.scmemory.model.element.node.ScNode;
+
+import java.util.Optional;
+
 /**
  * @author artrayme
  * 10/23/21
  */
 public class DefaultScContext {
+    private final ScMemory memory;
+
+    public DefaultScContext(ScMemory memory) {
+        this.memory = memory;
+    }
+
+    public ScNode createNode(NodeType type) {
+        return (ScNode) memory.createNodes(Optional.of(type).stream()).findFirst().get();
+    }
+
+
+
 
 }
