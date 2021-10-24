a9a1a90272cbc39bb145dc05a0ce9df601be9442
artrayme <artrayme@protonmail.com>
2021-10-24 20:54:24 +0300

    create mock implementations for testing

---
+++ src/test/java/context/mock/MockScNode.java
@@ -0,0 +1,28 @@
+package context.mock;
+
+import org.jmantic.scmemory.model.element.node.NodeType;
+import org.jmantic.scmemory.model.element.node.ScNode;
+
+/**
+ * @author artrayme
+ * 10/24/21
+ */
+class MockScNode implements ScNode {
+    private final Long address;
+    private final NodeType type;
+
+    MockScNode(Long address, NodeType type) {
+        this.address = address;
+        this.type = type;
+    }
+
+    @Override
+    public Long getAddress() {
+        return address;
+    }
+
+    @Override
+    public NodeType getType() {
+        return type;
+    }
+}
