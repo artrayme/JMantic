0725d3b992968921f42c5be02bd72b40b54aac69
artrayme <artrayme@protonmail.com>
2021-10-24 14:21:38 +0300

    add ScLinks with available content types

---
+++ src/main/java/org/jmantic/scmemory/model/element/link/ScLinkBinary.java
@@ -0,0 +1,9 @@
+package org.jmantic.scmemory.model.element.link;
+
+/**
+ * @author artrayme
+ * 10/24/21
+ */
+public interface ScLinkBinary<T> extends ScLink{
+    T getContent();
+}
---
+++ src/main/java/org/jmantic/scmemory/model/element/link/ScLinkInteger.java
@@ -0,0 +1,9 @@
+package org.jmantic.scmemory.model.element.link;
+
+/**
+ * @author artrayme
+ * 10/24/21
+ */
+public interface ScLinkInteger extends ScLink {
+    int getContent();
+}
