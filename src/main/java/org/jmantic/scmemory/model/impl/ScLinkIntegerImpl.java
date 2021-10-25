fb4bb23991b373f32db48215bd2272735c820f07
Миша <micha252282@gmail.com>
2021-10-25 18:48:15 +0300

    Beginning of work

---
+++ src/main/java/org/jmantic/scmemory/model/impl/ScLinkIntegerImpl.java
@@ -0,0 +1,16 @@
+package org.jmantic.scmemory.model.impl;
+
+import org.jmantic.scmemory.model.element.link.LinkType;
+import org.jmantic.scmemory.model.element.link.ScLink;
+
+class ScLinkIntegerImpl implements ScLink {
+    @Override
+    public Long getAddress() {
+        return null;
+    }
+
+    @Override
+    public LinkType getType() {
+        return null;
+    }
+}
--- src/main/java/org/jmantic/scmemory/model/websocket/request/CheckElementsRequest.java
+++
@@ -1,6 +0,0 @@
-package org.jmantic.scmemory.model.websocket.request;
-
-import org.jmantic.scmemory.model.websocket.ScRequest;
-
-public interface CheckElementsRequest extends ScRequest {
-}
