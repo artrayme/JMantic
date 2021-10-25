ec74953743af9c692f5cc5fd14b0c760f999a661
artrayme <artrayme@protonmail.com>
2021-10-25 15:03:00 +0300

    add tests for node creating

--- src/test/java/context/test/CreateNodeTest.java
+++ src/test/java/context/test/CreateNodeTest.java
@@ -2,9 +2,16 @@ package context.test;
 
 import context.mock.ScMemoryMock;
 import org.jmantic.api.context.DefaultScContext;
+import org.jmantic.scmemory.model.element.edge.EdgeType;
+import org.jmantic.scmemory.model.element.edge.ScEdge;
 import org.jmantic.scmemory.model.element.node.NodeType;
 import org.jmantic.scmemory.model.element.node.ScNode;
 import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author artrayme
@@ -15,7 +22,28 @@ public class CreateNodeTest {
     DefaultScContext scContext = new DefaultScContext(new ScMemoryMock());
 
     @Test
-    void createSimpleNode() {
+    void createNode() {
         ScNode node = scContext.createNode(NodeType.NODE);
+        assertEquals(NodeType.NODE, node.getType());
+    }
+
+    @Test
+    void createNodes() {
+        long size = 10;
+        var types = Stream.iterate(NodeType.NODE, e->NodeType.NODE).limit(size);
+        Stream<ScNode> nodes = scContext.createNodes(types);
+        nodes.forEach(e->{
+            assertEquals(e.getType(), NodeType.NODE);
+        });
+    }
+
+    @Test
+    void createEdge() {
+        ScNode first = scContext.createNode(NodeType.NODE);
+        ScNode second = scContext.createNode(NodeType.NODE);
+        ScEdge edge = scContext.createEdge(EdgeType.ACCESS, first, second);
+        assertEquals(edge.getType(), EdgeType.ACCESS);
+        assertEquals(edge.getFirst(), first);
+        assertEquals(edge.getSecond(), second);
     }
 }
