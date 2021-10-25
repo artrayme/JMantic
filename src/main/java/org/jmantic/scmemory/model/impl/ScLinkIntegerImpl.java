d2b279e08931753a2a9ec4b87f77aad741c60349
Миша <micha252282@gmail.com>
2021-10-25 19:02:30 +0300

    add sc_link_integer impl

--- src/main/java/org/jmantic/scmemory/model/impl/ScLinkIntegerImpl.java
+++ src/main/java/org/jmantic/scmemory/model/impl/ScLinkIntegerImpl.java
@@ -1,16 +1,37 @@
 package org.jmantic.scmemory.model.impl;
 
 import org.jmantic.scmemory.model.element.link.LinkType;
-import org.jmantic.scmemory.model.element.link.ScLink;
+import org.jmantic.scmemory.model.element.link.ScLinkInteger;
 
-class ScLinkIntegerImpl implements ScLink {
+class ScLinkIntegerImpl implements ScLinkInteger {
+    private final LinkType linkType;
+    private int content;
+    private long address;
+
+    public ScLinkIntegerImpl(LinkType linkType) {
+        this.linkType = linkType;
+    }
+
+    public void setAddress(long address) {
+        this.address = address;
+    }
+
+    public void setContent(int content) {
+        this.content = content;
+    }
+
     @Override
     public Long getAddress() {
-        return null;
+        return address;
     }
 
     @Override
     public LinkType getType() {
-        return null;
+        return linkType;
+    }
+
+    @Override
+    public int getContent() {
+        return content;
     }
 }
