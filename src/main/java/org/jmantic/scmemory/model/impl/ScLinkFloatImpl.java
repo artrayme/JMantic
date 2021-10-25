3212d5d7816aaae98c6bcfc67d376c72c69df0c0
Миша <micha252282@gmail.com>
2021-10-25 19:03:55 +0300

    sc_link_float  impl added

--- src/main/java/org/jmantic/scmemory/model/impl/ScLinkFloatImpl.java
+++ src/main/java/org/jmantic/scmemory/model/impl/ScLinkFloatImpl.java
@@ -1,4 +1,37 @@
 package org.jmantic.scmemory.model.impl;
 
-public class ScLinkFloatImpl {
+import org.jmantic.scmemory.model.element.link.LinkType;
+import org.jmantic.scmemory.model.element.link.ScLinkFloat;
+
+public class ScLinkFloatImpl implements ScLinkFloat {
+    private final LinkType linkType;
+    private float content;
+    private long address;
+
+    public ScLinkFloatImpl(LinkType linkType) {
+        this.linkType = linkType;
+    }
+
+    public void setAddress(long address) {
+        this.address = address;
+    }
+
+    public void setContent(int content) {
+        this.content = content;
+    }
+
+    @Override
+    public Long getAddress() {
+        return address;
+    }
+
+    @Override
+    public LinkType getType() {
+        return linkType;
+    }
+
+    @Override
+    public float getContent() {
+        return content;
+    }
 }
