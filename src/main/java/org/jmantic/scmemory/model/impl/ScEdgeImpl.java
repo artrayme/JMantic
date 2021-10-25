388f9e695d96bbd08f407fd8d43bbe6fc1d30bdc
Миша <micha252282@gmail.com>
2021-10-25 19:27:31 +0300

    sc_edge_impl added

--- src/main/java/org/jmantic/scmemory/model/impl/ScEdgeImpl.java
+++ src/main/java/org/jmantic/scmemory/model/impl/ScEdgeImpl.java
@@ -1,8 +1,52 @@
 package org.jmantic.scmemory.model.impl;
 
 
+import org.jmantic.scmemory.model.element.ScElement;
+import org.jmantic.scmemory.model.element.edge.EdgeType;
+import org.jmantic.scmemory.model.element.edge.ScEdge;
+
 /**
  * @author Michael
  */
-class ScEdgeImpl {
+class ScEdgeImpl implements ScEdge {
+    private final EdgeType edgeType;
+    private long address;
+    private ScElement sourceElement;
+    private ScElement targetElement;
+
+    public ScEdgeImpl(EdgeType edgeType) {
+        this.edgeType = edgeType;
+    }
+
+    public void setAddress(long address) {
+        this.address = address;
+    }
+
+    public void setSourceElement(ScElement sourceElement) {
+        this.sourceElement = sourceElement;
+    }
+
+    public void setTargetElement(ScElement targetElement) {
+        this.targetElement = targetElement;
+    }
+
+    @Override
+    public Long getAddress() {
+        return null;
+    }
+
+    @Override
+    public EdgeType getType() {
+        return null;
+    }
+
+    @Override
+    public ScElement getFirst() {
+        return null;
+    }
+
+    @Override
+    public ScElement getSecond() {
+        return null;
+    }
 }
