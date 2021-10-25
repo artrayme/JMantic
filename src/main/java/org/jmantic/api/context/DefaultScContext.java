e9dd983a6c5dd624d60ef9a46eb200bd98f0ce65
artrayme <artrayme@protonmail.com>
2021-10-25 15:01:53 +0300

    add method for creating multiple nodes

--- src/main/java/org/jmantic/api/context/DefaultScContext.java
+++ src/main/java/org/jmantic/api/context/DefaultScContext.java
@@ -1,10 +1,13 @@
 package org.jmantic.api.context;
 
 import org.jmantic.scmemory.model.ScMemory;
+import org.jmantic.scmemory.model.element.ScElement;
+import org.jmantic.scmemory.model.element.edge.EdgeType;
+import org.jmantic.scmemory.model.element.edge.ScEdge;
 import org.jmantic.scmemory.model.element.node.NodeType;
 import org.jmantic.scmemory.model.element.node.ScNode;
 
-import java.util.Optional;
+import java.util.stream.Stream;
 
 /**
  * @author artrayme
@@ -18,7 +21,26 @@ public class DefaultScContext {
     }
 
     public ScNode createNode(NodeType type) {
-        return (ScNode) memory.createNodes(Optional.of(type).stream()).findFirst().get();
+        var result = memory.createNodes(Stream.of(type)).findFirst();
+        if (result.isPresent()) {
+            return (ScNode) result.get();
+        }
+        //        ToDo normal Exceptions
+        throw new RuntimeException("Internal ScError");
+    }
+
+    public Stream<ScNode> createNodes(Stream<NodeType> types) {
+        var result = memory.createNodes(types);
+        return result.map(e -> (ScNode) e);
+    }
+
+    public ScEdge createEdge(EdgeType type, ScElement first, ScElement second) {
+        var edge = memory.createEdges(Stream.of(type), Stream.of(first), Stream.of(second)).findFirst();
+        if (edge.isPresent()) {
+            return (ScEdge) edge.get();
+        }
+        //        ToDo normal Exceptions
+        throw new RuntimeException("Internal ScError");
     }
 
 }
