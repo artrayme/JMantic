9df54e5a2221596d12548e63bb3d9db49a798080
artrayme <artrayme@protonmail.com>
2021-10-27 00:44:16 +0300

    change mock ScMemory to real in tests

--- src/test/java/context/test/CreateNodeTest.java
+++ src/test/java/context/test/CreateNodeTest.java
@@ -1,17 +1,18 @@
 package context.test;
 
-import context.mock.ScMemoryMock;
 import org.jmantic.api.context.DefaultScContext;
 import org.jmantic.scmemory.model.element.edge.EdgeType;
 import org.jmantic.scmemory.model.element.edge.ScEdge;
 import org.jmantic.scmemory.model.element.node.NodeType;
 import org.jmantic.scmemory.model.element.node.ScNode;
+import org.jmantic.scmemory.model.sync.SyncScMemory;
 import org.junit.jupiter.api.Test;
 
-import java.util.Objects;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author artrayme
@@ -19,20 +20,30 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 
 public class CreateNodeTest {
-    DefaultScContext scContext = new DefaultScContext(new ScMemoryMock());
+    DefaultScContext scContext = new DefaultScContext(new SyncScMemory(new URI("ws://localhost:8090/ws_json")));
+
+    public CreateNodeTest() throws URISyntaxException {
+
+    }
 
     @Test
     void createNode() {
+        long start = System.currentTimeMillis();
         ScNode node = scContext.createNode(NodeType.NODE);
+        System.err.println("Time = " + (System.currentTimeMillis()-start));
+//        System.err.println(node.getAddress());
         assertEquals(NodeType.NODE, node.getType());
     }
 
     @Test
     void createNodes() {
-        long size = 10;
-        var types = Stream.iterate(NodeType.NODE, e->NodeType.NODE).limit(size);
+        long size = 10000;
+        var types = Stream.iterate(NodeType.NODE, e -> NodeType.NODE).limit(size);
+        long start = System.currentTimeMillis();
         Stream<ScNode> nodes = scContext.createNodes(types);
-        nodes.forEach(e->{
+        System.err.println("Time = " + (System.currentTimeMillis()-start));
+        nodes.forEach(e -> {
+//            System.err.println(e.getAddress());
             assertEquals(e.getType(), NodeType.NODE);
         });
     }
