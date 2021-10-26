49e42af88d322b04b8cf2a06fd3c96b4497f0cb0
mixa1334 <m.kravczov@inbox.ru>
2021-10-26 19:41:02 +0300

    request sender with 1 method is done

---
+++ src/main/java/org/jmantic/scmemory/model/sync/RequestSenderImpl.java
@@ -0,0 +1,45 @@
+package org.jmantic.scmemory.model.sync;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import org.jmantic.scmemory.model.exception.ScMemoryException;
+import org.jmantic.scmemory.model.websocket.core.OstisClient;
+import org.jmantic.scmemory.model.websocket.message.ScMemoryView;
+import org.jmantic.scmemory.model.websocket.message.request.CreateScElRequest;
+import org.jmantic.scmemory.model.websocket.message.response.CreateScElResponse;
+import org.jmantic.scmemory.model.websocket.sender.RequestSender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Michael
+ */
+class RequestSenderImpl implements RequestSender {
+    private final static Logger logger = LoggerFactory.getLogger(OstisClient.class);
+    private final OstisClient client;
+    private final ObjectMapper mapper;
+    private final ObjectWriter writer;
+
+    public RequestSenderImpl(OstisClient client) {
+        this.client = client;
+        mapper = new ObjectMapper();
+        writer = mapper.writerWithView(ScMemoryView.Request.class);
+    }
+
+    @Override
+    public CreateScElResponse sendCreateElRequest(CreateScElRequest request) throws ScMemoryException {
+        try {
+            String jsonRequest = writer.writeValueAsString(request);
+            logger.info("request to ostis - " + request);
+            String msg = client.sendToOstis(jsonRequest);
+            CreateScElResponse response = mapper.readValue(msg, CreateScElResponseImpl.class);
+            logger.info("response from ostis - " + response);
+            return response;
+        } catch (JsonProcessingException e) {
+            String msg = "cant parse request - " + request;
+            logger.error(msg);
+            throw new ScMemoryException(msg, e);
+        }
+    }
+}
