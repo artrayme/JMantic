424e127fc05dd77a6d545b7912d13efdd2636999
mixa1334 <micha252282@gmail.com>
2021-10-26 13:58:50 +0300

    add request types

---
+++ src/main/java/org/jmantic/scmemory/model/websocket/request/RequestType.java
@@ -0,0 +1,21 @@
+package org.jmantic.scmemory.model.websocket.request;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+public enum RequestType {
+    CREATE_ELEMENTS("create_elements"),
+    CHECK_ELEMENTS("check_elements"),
+    DELETE_ELEMENTS("delete_elements"),
+    SEARCH_TEMPLATE("search_elements"),
+    GENERATE_TEMPLATE("generate_template"),
+    EVENTS("events"),
+    KEYNODES("keynodes"),
+    CONTENT("content");
+
+    @JsonValue
+    private final String type;
+
+    RequestType(String type) {
+        this.type = type;
+    }
+}
