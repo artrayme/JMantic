9c958e09128b9d9711a31e592e9884ac956999ef
artrayme <artrayme@protonmail.com>
2021-10-30 21:55:57 +0300

    add test for creating all nodes types

--- src/test/java/context/test/CreateNodeTest.java
+++ src/test/java/context/test/CreateNodeTest.java
@@ -1,14 +1,17 @@
 package context.test;
 
-import context.mock.ScMemoryMock;
 import org.jmantic.api.context.DefaultScContext;
 import org.jmantic.scmemory.model.element.edge.EdgeType;
 import org.jmantic.scmemory.model.element.edge.ScEdge;
 import org.jmantic.scmemory.model.element.node.NodeType;
 import org.jmantic.scmemory.model.element.node.ScNode;
+import org.jmantic.scmemory.websocketmemory.sync.SyncScMemory;
 import org.junit.jupiter.api.Test;
 
+import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,18 +22,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 
 public class CreateNodeTest {
-    DefaultScContext scContext = new DefaultScContext(new ScMemoryMock());
+    DefaultScContext scContext = new DefaultScContext(new SyncScMemory(new URI("ws://localhost:8090/ws_json")));
 
     public CreateNodeTest() throws URISyntaxException {
 
     }
 
+    //    ToDo test timeout
     @Test
     void createNode() {
-        long start = System.currentTimeMillis();
         ScNode node = scContext.createNode(NodeType.NODE);
-        System.err.println("Time = " + (System.currentTimeMillis()-start));
-//        System.err.println(node.getAddress());
         assertEquals(NodeType.NODE, node.getType());
     }
 
@@ -40,11 +41,20 @@ public class CreateNodeTest {
         var types = Stream.iterate(NodeType.NODE, e -> NodeType.NODE).limit(size);
         long start = System.currentTimeMillis();
         Stream<ScNode> nodes = scContext.createNodes(types);
-        System.err.println("Time = " + (System.currentTimeMillis()-start));
+        System.err.println("Time = " + (System.currentTimeMillis() - start));
         nodes.forEach(e -> {
-//            System.err.println(e.getAddress());
             assertEquals(e.getType(), NodeType.NODE);
         });
+    }
+
+    @Test
+    void createAllNodesTypes() {
+        Stream<ScNode> nodes = scContext.createNodes(Arrays.stream(NodeType.values()));
+        Iterator<ScNode> iter1 = nodes.iterator();
+        Iterator<NodeType> iter2 = Arrays.stream(NodeType.values()).iterator();
+        while (iter1.hasNext() && iter2.hasNext())
+            assertEquals(iter1.next().getType(), iter2.next());
+        assertEquals(iter1.hasNext(), iter2.hasNext());
     }
 
     @Test
