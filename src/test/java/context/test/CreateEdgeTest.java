0ca6d60804142175aaa43dfec7a80c18f69a5f8d
artrayme <artrayme@protonmail.com>
2021-11-02 18:29:12 +0300

    refactor tests

--- src/test/java/context/test/CreateEdgeTest.java
+++ src/test/java/context/test/CreateEdgeTest.java
@@ -1,7 +1,12 @@
 package context.test;
 
 import org.jmantic.api.context.DefaultScContext;
+import org.jmantic.scmemory.model.element.edge.EdgeType;
+import org.jmantic.scmemory.model.element.edge.ScEdge;
+import org.jmantic.scmemory.model.element.node.NodeType;
+import org.jmantic.scmemory.model.element.node.ScNode;
 import org.jmantic.scmemory.websocketmemory.sync.SyncScMemory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.net.URI;
@@ -15,20 +20,21 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * 10/30/21
  */
 public class CreateEdgeTest {
-    private final DefaultScContext scContext = new DefaultScContext(SyncScMemory.getSyncScMemory(new URI("ws://localhost:8090/ws_json")));
+    private DefaultScContext scContext;
 
-    public CreateEdgeTest() throws URISyntaxException {
-
+    @BeforeEach
+    public void setUp() throws URISyntaxException {
+        scContext = new DefaultScContext(new SyncScMemory(new URI("ws://localhost:8090/ws_json")));
     }
 
     @Test
     void createEdge() {
-        //        ScNode first = scContext.createNode(NodeType.NODE);
-        //        ScNode second = scContext.createNode(NodeType.NODE);
-        //        ScEdge edge = scContext.createEdge(EdgeType.ACCESS, first, second);
-        //        assertEquals(edge.getType(), EdgeType.ACCESS);
-        //        assertEquals(edge.getFirst(), first);
-        //        assertEquals(edge.getSecond(), second);
+        ScNode first = scContext.createNode(NodeType.NODE);
+        ScNode second = scContext.createNode(NodeType.NODE);
+        ScEdge edge = scContext.createEdge(EdgeType.ACCESS, first, second);
+//        assertEquals(edge.getType(), EdgeType.ACCESS);
+//        assertEquals(edge.getFirst(), first);
+//        assertEquals(edge.getSecond(), second);
         assertTrue(true);
     }
 }
