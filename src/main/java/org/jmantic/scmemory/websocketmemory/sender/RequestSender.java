02faecbc9e282d16a3abdb526a7ac4704a3121d3
artrayme <artrayme@protonmail.com>
2021-11-06 21:01:38 +0300

    feat: add sender method

--- src/main/java/org/jmantic/scmemory/websocketmemory/sender/RequestSender.java
+++ src/main/java/org/jmantic/scmemory/websocketmemory/sender/RequestSender.java
@@ -3,8 +3,10 @@ package org.jmantic.scmemory.websocketmemory.sender;
 import org.jmantic.scmemory.model.exception.ScMemoryException;
 import org.jmantic.scmemory.websocketmemory.message.request.CreateScElRequest;
 import org.jmantic.scmemory.websocketmemory.message.request.DeleteScElRequest;
+import org.jmantic.scmemory.websocketmemory.message.request.SearchByTemplateRequest;
 import org.jmantic.scmemory.websocketmemory.message.response.CreateScElResponse;
 import org.jmantic.scmemory.websocketmemory.message.response.DeleteScElResponse;
+import org.jmantic.scmemory.websocketmemory.message.response.SearchByTemplateResponse;
 
 /**
  * @author Michael
@@ -14,6 +16,8 @@ public interface RequestSender {
     CreateScElResponse sendCreateElRequest(CreateScElRequest request) throws ScMemoryException;
 
     DeleteScElResponse sendDeleteElRequest(DeleteScElRequest request) throws ScMemoryException;
+
+    SearchByTemplateResponse sendSearchByTemplateRequest(SearchByTemplateRequest request) throws ScMemoryException;
 
     // TODO: 6.11.21 more request
 }
