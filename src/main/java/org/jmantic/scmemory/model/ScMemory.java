2969b34737257f94715d82a3084828e1d61c5b00
artrayme <artrayme@protonmail.com>
2021-11-06 20:39:37 +0300

    refactor!: rename method, change signature with PECS, remove unused methods

--- src/main/java/org/jmantic/scmemory/model/ScMemory.java
+++ src/main/java/org/jmantic/scmemory/model/ScMemory.java
@@ -3,8 +3,14 @@ package org.jmantic.scmemory.model;
 import org.jmantic.scmemory.model.element.ScElement;
 import org.jmantic.scmemory.model.element.edge.EdgeType;
 import org.jmantic.scmemory.model.element.edge.ScEdge;
-import org.jmantic.scmemory.model.element.link.*;
+import org.jmantic.scmemory.model.element.link.LinkType;
+import org.jmantic.scmemory.model.element.link.ScLink;
+import org.jmantic.scmemory.model.element.link.ScLinkBinary;
+import org.jmantic.scmemory.model.element.link.ScLinkFloat;
+import org.jmantic.scmemory.model.element.link.ScLinkInteger;
+import org.jmantic.scmemory.model.element.link.ScLinkString;
 import org.jmantic.scmemory.model.element.node.NodeType;
+import org.jmantic.scmemory.model.element.node.ScNode;
 import org.jmantic.scmemory.model.exception.ScMemoryException;
 
 import java.util.stream.Stream;
@@ -28,22 +34,22 @@ public interface ScMemory {
 
     Stream<? extends ScLinkBinary> createBinaryLink(Stream<LinkType> elements, Stream<Object> content) throws ScMemoryException;
 
-    Stream<ScElement> checkElements(Stream<ScElement> elements) throws ScMemoryException;
+    //    Stream<ScElement> checkElements(Stream<ScElement> elements) throws ScMemoryException;
 
     boolean deleteElements(Stream<ScElement> elements) throws ScMemoryException;
 
-    Stream<ScEdge> findByTemplateF_A_A(ScElement element, EdgeType edgeType, NodeType nodeType) throws ScMemoryException;
+    Stream<? extends ScEdge> findByTemplateNodeEdgeNode(ScNode node, EdgeType edgeType, NodeType nodeType) throws ScMemoryException;
 
     //    ToDo generateTemplate
     //    ToDO events
     //    void addEventListener(ScEvent event, Function<ScElement, ScElement> predicate);
     //    void removeEventListener(ScEvent event);
 
-    ScElement findKeynode(String identifier) throws ScMemoryException;
+    //    ScElement findKeynode(String identifier) throws ScMemoryException;
 
-    ScElement resolvedKeynode(String identifier, NodeType type) throws ScMemoryException;
+    //    ScElement resolvedKeynode(String identifier, NodeType type) throws ScMemoryException;
 
-    Stream<ScLink> getLinkContent(Stream<ScLink> elements) throws ScMemoryException;
+    Stream<? extends ScLink> getLinkContent(Stream<? extends ScLink> elements) throws ScMemoryException;
 
-    Stream<Boolean> setLinkContent(Stream<ScLinkString> links, Stream<Object> content) throws ScMemoryException;
+    Stream<Boolean> setLinkContent(Stream<? extends ScLinkString> links, Stream<Object> content) throws ScMemoryException;
 }
---
+++ src/test/java/context/SearchByTemplateTest.java
@@ -0,0 +1,49 @@
+package context;
+
+import org.jmantic.api.context.DefaultScContext;
+import org.jmantic.scmemory.model.element.edge.EdgeType;
+import org.jmantic.scmemory.model.element.edge.ScEdge;
+import org.jmantic.scmemory.model.element.node.NodeType;
+import org.jmantic.scmemory.model.element.node.ScNode;
+import org.jmantic.scmemory.websocketmemory.sync.SyncScMemory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author artrayme
+ * 11/6/21
+ */
+public class SearchByTemplateTest {
+    private DefaultScContext scContext;
+
+    @BeforeEach
+    public void setUp() throws URISyntaxException {
+        scContext = new DefaultScContext(SyncScMemory.getSyncScMemory(new URI("ws://localhost:8090/ws_json")));
+    }
+
+    @Test
+    @Timeout(value = 5000, unit = TimeUnit.MILLISECONDS)
+    void createSingleTriple() {
+        ScNode source = scContext.createNode(NodeType.NODE);
+        ScNode target1 = scContext.createNode(NodeType.NODE);
+        //        ScNode target2 = scContext.createNode(NodeType.NODE);
+        ScEdge edge1 = scContext.createEdge(EdgeType.ACCESS, source, target1);
+        //        ScEdge edge2 = scContext.createEdge(EdgeType.ACCESS, source, target2);
+        var result = scContext.findAllConstructionsNodeEdgeNode(source, EdgeType.ACCESS, NodeType.NODE).toList();
+        assertEquals(source, result.get(0).getSource());
+        assertEquals(target1, result.get(0).getTarget());
+        assertEquals(edge1, result.get(0));
+
+        //        assertEquals(source, result.get(1).getSource());
+        //        assertEquals(target2, result.get(1).getTarget());
+        //        assertEquals(edge2, result.get(1));
+
+    }
+}
