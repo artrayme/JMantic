b7642eb0309977947f1499c3b1b3958877dc0280
artrayme <artrayme@protonmail.com>
2021-11-12 18:48:12 +0300

    feat:! add new implementation for OstisClient and exception for it

--- src/test/java/context/NodeOperationsTest.java
+++ src/test/java/context/NodeOperationsTest.java
@@ -3,6 +3,7 @@ package context;
 import org.jmantic.api.context.UncheckedScContext;
 import org.jmantic.scmemory.model.element.node.NodeType;
 import org.jmantic.scmemory.model.element.node.ScNode;
+import org.jmantic.scmemory.websocketmemory.sync.OstisClientSync;
 import org.jmantic.scmemory.websocketmemory.sync.SyncScMemory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -29,7 +30,8 @@ public class NodeOperationsTest {
 
     @BeforeEach
     void setUp() throws URISyntaxException {
-        scContext = new UncheckedScContext(SyncScMemory.getSyncScMemory(new URI("ws://localhost:8090/ws_json")));
+        scContext = new UncheckedScContext(new SyncScMemory(new OstisClientSync(new URI("ws://localhost:8090/ws_json"))));
+
     }
 
     @Test
--- src/test/java/context/ScMemoryTest.java
+++ src/test/java/context/ScMemoryTest.java
@@ -1,10 +1,12 @@
 package context;
 
 import org.jmantic.scmemory.model.exception.ScMemoryConfigurationException;
+import org.jmantic.scmemory.websocketmemory.sync.OstisClientSync;
 import org.jmantic.scmemory.websocketmemory.sync.SyncScMemory;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
+import java.net.URI;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -18,6 +20,7 @@ public class ScMemoryTest {
     @Test
     @Timeout(value = 5000, unit = TimeUnit.MILLISECONDS)
     public void testConfigurationException() {
-        assertThrows(ScMemoryConfigurationException.class, () -> SyncScMemory.getSyncScMemory(" "));
+//        assertThrows(ScMemoryConfigurationException.class, () -> new SyncScMemory(new OstisClientSync(new URI("ws://localhost:8090"))));
+
     }
 }
