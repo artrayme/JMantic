c79eace515fccaa764ffb37d94691b3b3f117292
mixa1334 <m.kravczov@inbox.ru>
2021-11-20 14:31:34 +0300

    refactor: ScSourceStruct and EndPointType

--- src/main/java/org/jmantic/scmemory/websocketmemory/sync/ScEntityImpl.java
+++ src/main/java/org/jmantic/scmemory/websocketmemory/sync/ScEntityImpl.java
@@ -3,6 +3,7 @@ package org.jmantic.scmemory.websocketmemory.sync;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
 import org.jmantic.scmemory.model.element.ScElement;
 import org.jmantic.scmemory.model.element.edge.EdgeType;
 import org.jmantic.scmemory.model.element.edge.ScEdge;
@@ -418,4 +419,62 @@ class ScEdgeImpl extends ScEntity implements ScEdge {
                 ", targetElement=" + targetElement +
                 '}';
     }
+}
+
+/**
+ * @author artrayme
+ * @since 0.2.0
+ */
+
+enum EdgeEndpointType {
+    ADDR("addr"),
+    REF("ref");
+
+    @JsonValue
+    private final String type;
+
+    EdgeEndpointType(String type) {
+        this.type = type;
+    }
+}
+
+/**
+ * @author artrayme
+ * @since 0.2.0
+ */
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+class EdgeSourceStruct {
+    @JsonProperty("type")
+    private final EdgeEndpointType type;
+    @JsonProperty("value")
+    private final Long value;
+
+    public EdgeSourceStruct(EdgeEndpointType type, Long value) {
+        this.type = type;
+        this.value = value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        EdgeSourceStruct that = (EdgeSourceStruct) o;
+        return type == that.type && Objects.equals(value, that.value);
+    }
+
+    @Override
+    public String toString() {
+        return "EdgeSourceStruct{" +
+                "type=" + type +
+                ", value=" + value +
+                '}';
+    }
+
 }
