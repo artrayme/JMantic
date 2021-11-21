c4277bbf9a28f9f7a5f8fd4c5a67616fd7fdfb12
artrayme <artrayme@protonmail.com>
2021-11-21 19:14:37 +0300

    test: add tests for template searching in the DefaultContext

---
+++ src/test/java/context/defaultcontext/SearchByTemplateTest.java
@@ -0,0 +1,46 @@
+package context.defaultcontext;
+
+import org.jmantic.api.context.DefaultScContext;
+import org.jmantic.scmemory.model.element.edge.EdgeType;
+import org.jmantic.scmemory.model.element.edge.ScEdge;
+import org.jmantic.scmemory.model.element.node.NodeType;
+import org.jmantic.scmemory.model.element.node.ScNode;
+import org.jmantic.scmemory.model.exception.ScMemoryException;
+import org.jmantic.scmemory.websocketmemory.sync.SyncOstisScMemory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * @author artrayme
+ * 11/21/21
+ */
+
+//ToDO exceptions test
+public class SearchByTemplateTest {
+    private DefaultScContext scContext;
+
+    @BeforeEach
+    public void setUp() throws URISyntaxException {
+        scContext = new DefaultScContext(new SyncOstisScMemory(new URI("ws://localhost:8090/ws_json")));
+    }
+
+    @Test
+    @Timeout(value = 5000, unit = TimeUnit.MILLISECONDS)
+    void createSingleTriple() throws ScMemoryException {
+        ScNode source = scContext.createNode(NodeType.NODE);
+        ScNode target1 = scContext.createNode(NodeType.NODE);
+        ScEdge edge1 = scContext.createEdge(EdgeType.ACCESS, source, target1);
+        var result = scContext.findAllConstructionsNodeEdgeNode(source, EdgeType.ACCESS, NodeType.NODE).toList();
+        assertEquals(source, result.get(0).getSource());
+        assertEquals(target1, result.get(0).getTarget());
+        assertEquals(edge1, result.get(0));
+
+    }
+}
