9580f962ea680b9573efc1ce1cb6f1860115cb59
artrayme <artrayme@protonmail.com>
2021-12-20 19:36:10 +0300

    test: add test for searching keynodes by identifiers in sc-memory implementation

---
+++ src/test/java/scmemory/ScMemoryFindKeynodeTest.java
@@ -0,0 +1,42 @@
+package scmemory;
+
+import org.jmantic.scmemory.model.ScMemory;
+import org.jmantic.scmemory.model.exception.ScMemoryException;
+import org.jmantic.scmemory.websocketmemory.sync.SyncOstisScMemory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ScMemoryFindKeynodeTest {
+    private ScMemory scMemory;
+
+    @BeforeEach
+    public void init() throws Exception {
+        scMemory = new SyncOstisScMemory(new URI("ws://localhost:8090/ws_json"));
+        scMemory.open();
+    }
+
+    @AfterEach
+    public void shutdown() throws Exception {
+        scMemory.close();
+    }
+
+    @Test
+    public void findKeynode() throws ScMemoryException {
+        var first = scMemory.findKeynodes(Stream.of("identifier*")).findFirst().get();
+        var second = scMemory.findKeynodes(Stream.of("identifier*")).findFirst().get();
+        assertEquals(first, second);
+    }
+
+    @Test
+    public void findMultipleKeynode() throws ScMemoryException {
+        var first = scMemory.findKeynodes(Stream.of("identifier*", "identifier*")).toList();
+        var second = scMemory.findKeynodes(Stream.of("identifier*", "identifier*")).toList();
+        assertEquals(first, second);
+    }
+}
