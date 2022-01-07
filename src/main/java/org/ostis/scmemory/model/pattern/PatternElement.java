d8a2b1632c15fd028030732cb776166d6a40959d
artrayme <artrayme@protonmail.com>
2022-01-07 03:28:15 +0300

    feat: make enum serializable for jackson-json

--- src/main/java/org/ostis/scmemory/model/pattern/PatternElement.java
+++ src/main/java/org/ostis/scmemory/model/pattern/PatternElement.java
@@ -1,11 +1,13 @@
 package org.ostis.scmemory.model.pattern;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum PatternElement {
     ALIAS("alias"),
     TYPE("type"),
     ADDR("addr");
 
-
+    @JsonValue
     private final String type;
 
     PatternElement(String type) {
