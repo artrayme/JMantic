a7b2bccd212e3c2048feaad702cd1560af00fdbd
mixa1334 <m.kravczov@inbox.ru>
2022-02-27 14:45:43 +0300

    fix(src/main/java/org/ostis/scmemory/websocketmemory/memory/SyncOstisScMemory.java): bugs fixed

--- src/main/java/org/ostis/scmemory/websocketmemory/memory/SyncOstisScMemory.java
+++ src/main/java/org/ostis/scmemory/websocketmemory/memory/SyncOstisScMemory.java
@@ -68,8 +68,10 @@ import org.ostis.scmemory.websocketmemory.message.response.SetLinkContentRespons
 import org.ostis.scmemory.websocketmemory.sender.RequestSender;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -477,9 +479,49 @@ public class SyncOstisScMemory implements ScMemory {
     @Override
     public Stream<Boolean> setBinaryLinkContent(Stream<? extends ScLinkBinary> links,
                                                 Stream<ByteArrayOutputStream> content) throws ScMemoryException {
-        return setLinkContent(
-                links,
-                content);
+        SetLinkContentRequestImpl request = new SetLinkContentRequestImpl();
+        List<? extends ScLinkBinary> linksList = links.toList();
+        List<ByteArrayOutputStream> contentList = content.toList();
+        if (linksList.size() != contentList.size()) {
+            throw new IllegalArgumentException("The length of the passed lists are not the same." + "linksList.size = " + linksList.size() + ", contentList.size = " + contentList.size());
+        }
+        Iterator<? extends ScLinkBinary> linksIter = linksList.iterator();
+        Iterator<ByteArrayOutputStream> contentIter = contentList.iterator();
+        List<ScLinkBinary> linksWithoutContent = new ArrayList<>();
+        List<String> contentWithoutLink = new ArrayList<>();
+        while (linksIter.hasNext()) {
+            ScLinkBinary link = linksIter.next();
+            linksWithoutContent.add(link);
+            String data = Base64.getEncoder()
+                                .encodeToString(contentIter.next()
+                                                           .toByteArray());
+            contentWithoutLink.add(data);
+            request.addToRequest(
+                    link,
+                    data);
+        }
+
+        SetLinkContentResponse response = requestSender.sendSetLinkContentRequest(request);
+
+        if (!response.getResponseStatus()) {
+            throw new ScMemoryException("the response status is FALSE");
+        }
+        List<Boolean> statusOfOperation = response.getOperationStatus();
+        for (int i = 0; i < statusOfOperation.size(); i++) {
+            boolean status = statusOfOperation.get(i);
+            if (status) {
+                ScLinkBinary link = linksWithoutContent.get(i);
+                String data = contentWithoutLink.get(i);
+                try {
+                    ((ScLinkBinaryImpl) link).setContent(data);
+                } catch (IOException e) {
+                    throw new ScMemoryException(
+                            "Unable to parse string to binary representation",
+                            e);
+                }
+            }
+        }
+        return statusOfOperation.stream();
     }
 
     @Override
@@ -701,7 +743,15 @@ public class SyncOstisScMemory implements ScMemory {
                     case FLOAT -> ((ScLinkFloatImpl) link).setContent((float) data);
                     case INT -> ((ScLinkIntegerImpl) link).setContent((int) data);
                     case STRING -> ((ScLinkStringImpl) link).setContent((String) data);
-                    case BINARY -> ((ScLinkBinaryImpl) link).setContent((String) data);
+                    case BINARY -> {
+                        try {
+                            ((ScLinkBinaryImpl) link).setContent((String) data);
+                        } catch (IOException e) {
+                            throw new ScMemoryException(
+                                    "Unable to parse string to binary representation",
+                                    e);
+                        }
+                    }
                 }
             }
         }
@@ -746,8 +796,14 @@ public class SyncOstisScMemory implements ScMemory {
                     }
                     case BINARY -> {
                         String content = (String) value;
-                        result.add(content);
-                        ((ScLinkBinaryImpl) link).setContent(content);
+                        try {
+                            ((ScLinkBinaryImpl) link).setContent(content);
+                            result.add(((ScLinkBinaryImpl) link).getContent());
+                        } catch (IOException e) {
+                            throw new ScMemoryException(
+                                    "Unable to parse string to binary representation",
+                                    e);
+                        }
                     }
                 }
             }
@@ -805,7 +861,13 @@ public class SyncOstisScMemory implements ScMemory {
                     ScLinkBinaryImpl scLinkBinary = new ScLinkBinaryImpl(
                             type,
                             links.get(i));
-                    scLinkBinary.setContent(content);
+                    try {
+                        scLinkBinary.setContent(content);
+                    } catch (IOException e) {
+                        throw new ScMemoryException(
+                                "Unable to parse string to binary representation",
+                                e);
+                    }
                     result.add(scLinkBinary);
                 }
                 default -> throw new IllegalArgumentException("unknown type of content");
