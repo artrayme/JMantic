a37f4687e835d81170e5b66eb0a58c41e487d4d5
artrayme <artrayme@protonmail.com>
2022-04-10 03:07:18 +0300

    feat: return java9 module

---
+++ src/main/java/module-info.java
@@ -0,0 +1,30 @@
+module JMantic.main {
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.databind;
+    requires org.slf4j;
+    requires jakarta.websocket;
+
+    //    exporting usable public api
+    exports org.ostis.api.context;
+    exports org.ostis.scmemory.websocketmemory.util.api;
+
+    //    exporting main interfaces and abstractions
+    exports org.ostis.scmemory.model;
+    exports org.ostis.scmemory.model.exception;
+    exports org.ostis.scmemory.model.event;
+    exports org.ostis.scmemory.model.element;
+    exports org.ostis.scmemory.model.element.node;
+    exports org.ostis.scmemory.model.element.link;
+    exports org.ostis.scmemory.model.element.edge;
+    exports org.ostis.scmemory.model.pattern;
+    exports org.ostis.scmemory.model.pattern.element;
+    exports org.ostis.scmemory.model.pattern.factory;
+    exports org.ostis.scmemory.model.pattern.pattern3;
+    exports org.ostis.scmemory.model.pattern.pattern5;
+
+    //    exporting sync sc-memory implementation
+    exports org.ostis.scmemory.websocketmemory.memory;
+    exports org.ostis.scmemory.websocketmemory.memory.pattern;
+    exports org.ostis.scmemory.websocketmemory.memory.pattern.element;
+
+}
